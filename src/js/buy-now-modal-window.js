8cb6b3815f27ea38a971a5fcd7da9aca41ad7be2
Bogdan <bogdanberezka@gmail.com>
2022-09-06 20:26:08 +0300

    make corrections

--- src/js/buy-now-modal-window.js
+++ src/js/buy-now-modal-window.js
@@ -18,22 +18,3 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-buy-now-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-buy-now-modal-close]'),
-//     modalItems: document.querySelectorAll('[data-modal-item]'),
-//     modal: document.querySelector('[data-buy-now-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-//   refs.modalItems.forEach(item => {
-//     item.addEventListener('click', toggleModal);
-//   });
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//   }
-// })();
--- src/js/smooth-anchors.js
+++ src/js/smooth-anchors.js
@@ -2,8 +2,7 @@ const anchors = document.querySelectorAll('a[href^="#"]');
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault(); // Предотвратить стандартное поведение ссылок
-    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    e.preventDefault();
     const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
     // Плавная прокрутка до элемента с id = href у ссылки
     document.querySelector(goto).scrollIntoView({
