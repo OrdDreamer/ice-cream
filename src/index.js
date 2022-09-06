73d12f5071fa7a2255c17343d54e28b20138f9f7
Bogdan <bogdanberezka@gmail.com>
2022-09-06 11:28:21 +0300

    add js smooth scroll

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/mobile-menu';
+import './js/smooth-anchors';
 import './js/buy-now-modal-window';
 import './js/about-modal-window';
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -7,8 +7,8 @@
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
-  
+
   function toggleModal() {
-   refs.menu.classList.toggle('is-open');
+    refs.menu.classList.toggle('is-open');
   }
 })();
